792b39aac9afec42713fe11cdd8c5ca96f0638a1
Allen Z Young <allenzyoung97@gmail.com>
2015-12-14 08:49:21 +0800

    Update
    Trans comments.

--- Codes/1107-1.rkt
+++ Codes/1107-1.rkt
@@ -1,24 +1,24 @@
 #lang planet neil/sicp
 (* 1 2.0 3)
-(+ (* 2 3) (- 10 6 1)) ;这TM是注释你敢信？
+(+ (* 2 3) (- 10 6 1)) ;the comments grammer is so odd...
 
 (+ 5 3
    (- 4 2 (* 7 2)
       (/ 3.0 5 )
       )
-   );要我说，我很难理解这种所谓的“美观打印”。。。
-(define size 2);命名方式也很奇怪。。。
+   );it's suggested to coding in this "style".
+(define size 2);which means "size = 2"
 (* 5 size)
 (define area (* size size))
 area
 
 (define (square x) (* x x))
-(square 5) ;过程定义，也是一种抽象
+(square 5) ;define a process, in my view, a function.
 
 (define (sos x y) (+ (square x) (square y)))
 (sos 3 4)
 (define (f a)
-  (sos (+ a 1) (* a 2))) ; 一组较为复杂的过程定义——复合过程
+  (sos (+ a 1) (* a 2))) ; Complex process.
 (f 5)
 
 
